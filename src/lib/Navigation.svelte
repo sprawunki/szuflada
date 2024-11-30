943a6e1da931bfca152e86f9bc4bebd4c2570834
Maciej Lewkowicz <maciek@lewkowi.cz>
2024-11-30 16:27:06 +0100

    Restyle main navigation
    Change-Id: Ifb1db6df4f274f6e6d44a9fcab824bb121034547

--- src/lib/Navigation.svelte
+++ src/lib/Navigation.svelte
@@ -1,47 +1,46 @@
 <script>
-    import { base } from "$app/paths"
+  import { base } from "$app/paths";
 </script>
 
 <div class="navigation" id="navigation">
-    <ul>
-        <li>
-            <a href="{base}/">Bookmarks</a>
-        </li>
-        <li>
-            <a href="{base}/products/">Products</a>
-        </li>
-        <li>
-            <a href="{base}/tasks/">Tasks</a>
-        </li>
-    </ul>
+  <ul>
+    <li>
+      <a href="{base}/">Bookmarks</a>
+    </li>
+    <li>
+      <a href="{base}/products/">Products</a>
+    </li>
+    <li>
+      <a href="{base}/tasks/">Tasks</a>
+    </li>
+  </ul>
 </div>
 
 <style>
-    .navigation {
-        background: #eee;
-        color: #111;
-        width: 100%;
-        padding: 0.5em 0;
-    }
+  .navigation {
+    background: var(--background-color);
+    color: var(--foreground-color);
+    width: 100%;
+    padding: 0;
+  }
 
-    .navigation a {
-        color: inherit;
-        padding: 0.25em;
-        margin: 0;
-        display: block;
-    }
+  .navigation a {
+    padding: 0.5lh;
+    margin: 0;
+    display: block;
+  }
 
-    .navigation ul {
-        margin: 0;
-        padding: 0;
-        display: flex;
-        flex-direction: row;
-        justify-content: space-evenly;
-    }
+  .navigation ul {
+    margin: 0;
+    padding: 0;
+    display: flex;
+    flex-direction: row;
+    justify-content: space-evenly;
+  }
 
-    .navigation li {
-        list-style: none;
-        margin: 0;
-        padding: 0;
-    }
+  .navigation li {
+    list-style: none;
+    margin: 0;
+    padding: 0;
+  }
 </style>

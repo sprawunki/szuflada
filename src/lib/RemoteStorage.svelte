22c9feaddbe183c8cb066cf9b588dabe73f4d452
Maciej Lewkowicz <maciek@lewkowi.cz>
2024-11-30 16:43:36 +0100

    Restyle app header
    Change-Id: I8be17d38a601196ac91d5b984757552824dd0e4f

--- src/lib/RemoteStorage.svelte
+++ src/lib/RemoteStorage.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-  import { onMount, onDestroy } from 'svelte';
+  import { onMount, onDestroy } from "svelte";
   import {
     connect,
     disconnect,
@@ -8,140 +8,148 @@
     getBookmarkList,
     getTasks,
     getTaskList,
-  } from '$lib/remotestorage.ts'
+  } from "$lib/remotestorage.ts";
 
-  import {
-    bookmarks,
-    tasks,
-    productProgress,
-    products,
-  } from '$lib/store'
-  import * as jsonld from 'jsonld'
+  import { bookmarks, tasks, productProgress, products } from "$lib/store";
+  import * as jsonld from "jsonld";
 
-  import { v5 as uuidv5 } from 'uuid'
-  const NS_BOOKMARK = uuidv5("https://szuflada.app/bookmark/", uuidv5.URL)
+  import { v5 as uuidv5 } from "uuid";
+  const NS_BOOKMARK = uuidv5("https://szuflada.app/bookmark/", uuidv5.URL);
 
-  const workers = {}
+  const workers = {};
 
-  let remote = remoteStorage.remote
+  let remote = remoteStorage.remote;
 
   remoteStorage.on("connected", () => {
-    remote = remoteStorage.remote
-  })
+    remote = remoteStorage.remote;
+  });
 
   remoteStorage.on("disconnected", () => {
-    remote = remoteStorage.remote
-  })
+    remote = remoteStorage.remote;
+  });
 
-  const handleConnect = (event)  => {
+  const handleConnect = (event) => {
     const formData = new FormData(event.target);
-    connect(formData.get('address'))
-  }
+    connect(formData.get("address"));
+  };
 
   const handleDisconnect = () => {
-    disconnect()
-  }
+    disconnect();
+  };
 
   onMount(async () => {
-    const IndexWorker = await import('$lib/index.worker?worker')
-    workers.index = new IndexWorker.default()
+    const IndexWorker = await import("$lib/index.worker?worker");
+    workers.index = new IndexWorker.default();
 
     workers.index.onmessage = (event) => {
-      if (event.data.hasOwnProperty('progress')) {
-        productProgress.set(event.data.progress)
+      if (event.data.hasOwnProperty("progress")) {
+        productProgress.set(event.data.progress);
       }
 
-      if(event.data.hasOwnProperty('products')) {
-        products.set(Object.fromEntries((event.data.products['schema:demands'] ?? []).map(item => [item['@id'], item])))
+      if (event.data.hasOwnProperty("products")) {
+        products.set(
+          Object.fromEntries(
+            (event.data.products["schema:demands"] ?? []).map((item) => [
+              item["@id"],
+              item,
+            ]),
+          ),
+        );
       }
-    }
+    };
 
-    remoteStorage['szuflada.app/bookmark'].getPrivateClient().on('change', (event: any) => {
-      if (event.origin !== 'local') {
-        bookmarks.update(bookmark => {
-          if(event.oldValue && event.oldValue['@id']) {
-            delete bookmark[event.oldValue['@id']];
-          }
+    remoteStorage["szuflada.app/bookmark"]
+      .getPrivateClient()
+      .on("change", (event: any) => {
+        if (event.origin !== "local") {
+          bookmarks.update((bookmark) => {
+            if (event.oldValue && event.oldValue["@id"]) {
+              delete bookmark[event.oldValue["@id"]];
+            }
 
-          if(event.newValue && event.newValue['@id']) {
-            bookmark[event.newValue['@id']] = event.newValue;
-          }
+            if (event.newValue && event.newValue["@id"]) {
+              bookmark[event.newValue["@id"]] = event.newValue;
+            }
 
-          return bookmark;
-        })
-      }
+            return bookmark;
+          });
+        }
 
-      workers.index.postMessage(event)
-    })
+        workers.index.postMessage(event);
+      });
 
-    remoteStorage['szuflada.app/task'].getPrivateClient().on('change', (event: any) => {
-      if (event.origin !== 'local') {
-        tasks.update(task => {
-          if(event.oldValue && event.oldValue['@id']) {
-            delete task[event.oldValue['@id']];
-          }
+    remoteStorage["szuflada.app/task"]
+      .getPrivateClient()
+      .on("change", (event: any) => {
+        if (event.origin !== "local") {
+          tasks.update((task) => {
+            if (event.oldValue && event.oldValue["@id"]) {
+              delete task[event.oldValue["@id"]];
+            }
 
-          if(event.newValue && event.newValue['@id']) {
-            task[event.newValue['@id']] = event.newValue;
-          }
+            if (event.newValue && event.newValue["@id"]) {
+              task[event.newValue["@id"]] = event.newValue;
+            }
 
-          return task;
-        })
-      }
-    })
+            return task;
+          });
+        }
+      });
 
     remoteStorage.on("ready", async () => {
       const parsedUrl = new URL(window.location);
 
-      const title = parsedUrl.searchParams.get('title');
-      const text = parsedUrl.searchParams.get('text');
-      const url = parsedUrl.searchParams.get('url');
+      const title = parsedUrl.searchParams.get("title");
+      const text = parsedUrl.searchParams.get("text");
+      const url = parsedUrl.searchParams.get("url");
 
       const getUrlFromCandidate = (candidate) => {
         try {
-          return new URL(candidate).toString()
+          return new URL(candidate).toString();
         } catch (e) {
-          return null
+          return null;
         }
-      }
+      };
 
-      const finalUrl = getUrlFromCandidate(url) || getUrlFromCandidate(text)
+      const finalUrl = getUrlFromCandidate(url) || getUrlFromCandidate(text);
 
       if (finalUrl) {
-        console.log("SAVING", finalUrl)
-        await remoteStorage['szuflada.app/bookmark'].save({
+        console.log("SAVING", finalUrl);
+        await remoteStorage["szuflada.app/bookmark"].save({
           "@id": `urn:uuid:${uuidv5(finalUrl, NS_BOOKMARK)}`,
           "@type": "http://www.w3.org/2002/01/bookmark#Bookmark",
           "http://purl.org/dc/elements/1.1/#created": new Date().toISOString(),
           "http://purl.org/dc/elements/1.1/#date": new Date().toISOString(),
-          "http://www.w3.org/2002/01/bookmark#title": [{
-            "@value": title,
-          }],
+          "http://www.w3.org/2002/01/bookmark#title": [
+            {
+              "@value": title,
+            },
+          ],
           "http://www.w3.org/2002/01/bookmark#recalls": {
-            "@id": finalUrl
-          }
-        })
+            "@id": finalUrl,
+          },
+        });
       }
 
       await getTasks()
         .then((allTasks: any) => {
-            $tasks = { ...allTasks }
+          $tasks = { ...allTasks };
         })
-        .then(() => getTaskList())
+        .then(() => getTaskList());
 
       await getBookmarks()
         .then((allBookmarks: any) => {
-            $bookmarks = { ...allBookmarks }
+          $bookmarks = { ...allBookmarks };
         })
-        .then(() => getBookmarkList())
-    })
-  })
+        .then(() => getBookmarkList());
+    });
+  });
 
   onDestroy(() => {
     for (const worker in workers) {
-      workers[worker].terminate()
+      workers[worker].terminate();
     }
-  })
+  });
 </script>
 
 <div class="remotestorage">
@@ -150,8 +158,8 @@
     <button on:click={handleDisconnect}>Disconnect</button>
   {:else}
     <form on:submit|preventDefault={handleConnect}>
-      <input type="email" id="address" name="address" value=""/>
-      <input type="submit" value="Connect"/>
+      <input type="email" id="address" name="address" value="" />
+      <input type="submit" value="Connect" />
     </form>
   {/if}
 </div>
@@ -160,15 +168,47 @@
   .remotestorage {
     display: flex;
     flex-direction: row;
-    justify-content: center;
-    padding: 0 0.25rem;
+    justify-content: space-between;
+    align-items: center;
+    padding: 0 0 0 1lh;
     overflow: hidden;
+    flex-grow: 1;
+  }
+
+  form {
+    display: flex;
+    flex: 1;
   }
 
   .current-user {
-    padding: 0.25rem;
     hyphens: auto;
     overflow: hidden;
     text-overflow: ellipsis;
+    flex: 1;
+  }
+
+  input {
+    background: var(--delicate-gray);
+    color: var(--foreground-color);
+    border: none;
+    flex: 1;
+    width: 100%;
+  }
+
+  button,
+  input[type="submit"] {
+    cursor: pointer;
+    appearance: none;
+    background: var(--accent-color);
+    color: var(--background-color);
+    border: none;
+    padding: 0.5lh;
+    flex: 0;
+  }
+
+  button:hover,
+  input[type="submit"]:hover {
+    background: var(--background-color);
+    color: var(--accent-color);
   }
 </style>

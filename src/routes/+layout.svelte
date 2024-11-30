50f088c1ae0f327596d48abeb8174bba2383d887
Maciej Lewkowicz <maciek@lewkowi.cz>
2024-11-30 16:22:55 +0100

    Restyle app shell
    Change-Id: Id28a89ba9304d0d2ad6d6b5a4fd80f178bfbe5fe

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,32 +1,30 @@
 <script lang="ts">
-	import '../app.css';
-	import { onMount } from 'svelte';
+  import "../app.css";
+  import { onMount } from "svelte";
 
-	import Navigation from "$lib/Navigation.svelte"
+  import Navigation from "$lib/Navigation.svelte";
 
-	let RemoteStorage;
+  let RemoteStorage;
 
-	onMount(async () => {
-		const remotestorage = await import('$lib/RemoteStorage.svelte');
-		RemoteStorage = remotestorage.default;
-	})
+  onMount(async () => {
+    const remotestorage = await import("$lib/RemoteStorage.svelte");
+    RemoteStorage = remotestorage.default;
+  });
 </script>
 
 <header>
-	<div class="container">
-		<img src="/szuflada/favicon.png" alt="Szuflada" width="32" height="32" />
-		<svelte:component this={RemoteStorage} />
-	</div>
+  <div class="container">
+    <img src="/szuflada/favicon.png" alt="Szuflada" width="32" height="32" />
+    <svelte:component this={RemoteStorage} />
+  </div>
+  <Navigation />
+  <slot name="header"></slot>
 </header>
 
-<div class="container">
-	<Navigation/>
-</div>
-
 <main>
-	<div class="container">
-		<slot/>
-	</div>
+  <div class="container">
+    <slot />
+  </div>
 </main>
 
 <!-- <footer>
@@ -36,52 +34,63 @@
 </footer> -->
 
 <style>
-	header {
-		background: #077;
-		color: #eee;
-		padding: 0.25rem 0;
-		z-index: 1;
-	}
+  header {
+    background: var(--primary-color);
+    color: var(--foreground-color);
+    padding: 0;
+    max-width: 100%;
+    z-index: 1;
+    position: sticky;
+    top: 0;
+    right: 0;
+    left: 0;
+    overflow: hidden;
+  }
 
-	header .container {
-		display: flex;
-		justify-content: space-between;
-		align-items: center;
-	}
+  header .container {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding: 0.5rlh;
+  }
 
-	main {
-		flex: 1;
-		display: flex;
-		flex-direction: column;
-		justify-content: flex-end;
-		margin: 0;
-		padding: 0;
-		box-sizing: border-box;
-		position: relative;
-		overflow: scroll;
-	}
+  main {
+    flex: 1;
+    display: flex;
+    flex-direction: column;
+    align-items: stretch;
+    justify-content: space-between;
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    position: relative;
+    overflow: hidden scroll;
+    max-width: 100%;
+    max-height: 100%;
+  }
 
-	main .container {
-		display: flex;
-		flex-direction: column;
-		min-height: 100%;
-	}
+  main .container {
+    flex: 1;
+    display: flex;
+    flex-direction: column;
+    max-height: 100%;
+  }
 
-	footer {
-		display: flex;
-		flex-direction: column;
-		justify-content: center;
-		align-items: center;
-		padding: 0.5rem 0;
-		position: sticky;
-		right: 0;
-		bottom: 0;
-		left: 0;
-		background: #111;
-		color: #eee;
-	}
+  footer {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    padding: 0.5rem 0;
+    position: sticky;
+    right: 0;
+    bottom: 0;
+    left: 0;
+    background: #111;
+    color: #eee;
+  }
 
-	footer a {
-		font-weight: bold;
-	}
+  footer a {
+    font-weight: bold;
+  }
 </style>
